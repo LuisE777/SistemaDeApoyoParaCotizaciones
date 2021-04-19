bc718c5312354703be2984dad40217907183dd38
JMauricioQuinonesVargas <201702063@est.umss.edu>
2021-04-19 10:58:20 -0400

    agregado form rol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroUsuarioComponent } from './registros/registro-usuario/registro-usuario.component';
 import { RegistroUnidadComponent } from './registros/registro-unidad/registro-unidad.component';
+import { RegistroRolComponent } from './registros/registro-rol/registro-rol.component';
 import { LoginComponent } from './login/login.component';
 import { FormSolicitudComponent } from './form-solicitud/form-solicitud.component';
 import { RegistroItemComponent } from './registros/registro-item/registro-item.component';
@@ -21,7 +22,8 @@ import { MatTableModule } from '@angular/material/table';
 import { AutocompletarComponent } from './form-solicitud/diagitem/autocompletar/autocompletar.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import { RegistroRolComponent } from './registros/registro-rol/registro-rol.component';
+
+
 
 //Hasta aquis
 
--- src/app/registros/registro-rol/registro-rol.component.ts
+++ src/app/registros/registro-rol/registro-rol.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+ 
 
 @Component({
   selector: 'app-registro-rol',
@@ -11,5 +13,33 @@ export class RegistroRolComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  nombreRol = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]);
+  descripcionRol = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*') ]);
 
+  getErrorMessage(c: Number) {
+    switch (c) {
+      case 1:
+        if (this.nombreRol.hasError('required')) {
+          return 'El valor es requerido';
+        }     
+        return 'Solo se admiten letras.'; 
+        break;
+      case 2:
+        if (this.descripcionRol.hasError('required')) {
+          return 'El valor es requerido';
+        }     
+        return 'Solo se admiten letras.'; 
+        break;
+      default:
+        return '';
+        break;
+    }
+
+               
+  }
+
+  guardarRol () {
+    console.log(this.nombreRol.value);
+    console.log(this.descripcionRol.value);
+  }
 }
