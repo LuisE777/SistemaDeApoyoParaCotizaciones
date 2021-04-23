234570845cdf1f981e50fc45e8a7b0de2c83340c
JMauricioQuinonesVargas <201702063@est.umss.edu>
2021-04-23 11:32:13 -0400

    cambiado ruta de api

--- src/app/services/solicitud.service.ts
+++ src/app/services/solicitud.service.ts
@@ -10,8 +10,8 @@ export class SolicitudService {
 
   constructor(private http: HttpClient) { }
 
-  //URL_API='https://apiser-vicios.herokuapp.com/api/auth';
-  URL_API='http://127.0.0.1:8000/api/auth';
+  URL_API='https://apiser-vicios.herokuapp.com/api/auth';
+  //URL_API='http://127.0.0.1:8000/api/auth';
 
   obtenerSolicitud(){
     return this.http.get<Solicitud[]>(this.URL_API+ '/solicitudes');
