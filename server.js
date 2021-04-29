bbef4a917755300f4b0e4b472d693df26db19b47
Neo <enrique.vallejos.777@gmail.com>
2021-04-29 09:24:22 -0400

    initial heroku deploy

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/sistema-de-apoyo-para-cotizaciones'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/sistema-de-apoyo-para-cotizaciones/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5080);
