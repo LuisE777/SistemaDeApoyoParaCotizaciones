70faf1ee58e2277ba94d09483e63c9f4421b8282
Neo <enrique.vallejos.777@gmail.com>
2021-05-02 17:04:08 -0400

    Otra configuracion para tratar de deployar

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/sistema-de-apoyo-para-cotizaciones'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/sistema-de-apoyo-para-cotizaciones/index.html'));});
-app.listen(process.env.PORT || 5080);
+app.use(express.static('./dist/sistema-de-apoyo-para-cotizaciones'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/sistema-de-apoyo-para-cotizaciones/'}),
+);
+app.listen(process.env.PORT || 8080);
