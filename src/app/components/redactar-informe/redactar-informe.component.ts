cae97fb2f24704afe4495e55e67a83128f421f0b
tholliver <michaelmmv@outlook.com>
2021-06-12 12:11:05 -0400

    Link to Redactar-Informe component

---
+++ src/app/components/redactar-informe/redactar-informe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redactar-informe',
+  templateUrl: './redactar-informe.component.html',
+  styleUrls: ['./redactar-informe.component.css']
+})
+export class RedactarInformeComponent implements OnInit {
+  //RedactarInformeComponent
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
