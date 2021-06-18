fba8a80c5ce9c1c89d114beaa3fefed1d626842e
JMauricioQuinonesVargas <201702063@est.umss.edu>
2021-06-18 10:42:58 -0400

    Eliminar rol

--- src/app/lista-roles/lista-roles.component.ts
+++ src/app/lista-roles/lista-roles.component.ts
@@ -14,8 +14,9 @@ import { UsuarioService } from './../services/usuario.service';
 })
 export class ListaRolesComponent implements OnInit {
   filterPost = '';
-  RolesUmss:Roles[];
+  RolesUmss:Roles[]=[];
   rolAEditar:Roles;
+  rolAEliminar:Roles;
   constructor( public _usuarioService:UsuarioService, public rolService: RolService, public router: Router) { }
 
   nombreRol = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]);
@@ -29,6 +30,7 @@ export class ListaRolesComponent implements OnInit {
       console.log(this.RolesUmss);
     })
   }
+  
   getErrorMessage(c: Number) {   
     switch (c) {
       case 1:
@@ -54,6 +56,28 @@ export class ListaRolesComponent implements OnInit {
     this.nombreRol.setValue(this.rolAEditar.rolnom);
     this.descripcionRol.setValue(this.rolAEditar.descrip);
   }
+
+  eliminarRol(rol: Roles, index: number){   
+    Swal.fire({
+      title: 'Seguro quiere eliminar este registro?',
+      showDenyButton: true,
+      confirmButtonText: `Eliminar`,
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        this.RolesUmss.splice(index, 1);
+        this.RolesUmss = [...this.RolesUmss];
+        this.rolService.eliminarRol(rol.id).subscribe(() => {
+            console.log(this.RolesUmss);
+          });
+        Swal.fire('Eliminado!', '', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('No se eliminó el registro', '', 'info')
+      }
+    });
+  }
+  
 
   editarRol(){
     if(!this.nombreRol.invalid && !this.descripcionRol.invalid){
--- src/app/services/rol.service.ts
+++ src/app/services/rol.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Rol } from '../models/rol.model'
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,7 +10,7 @@ export class RolService {
 
   constructor(private http: HttpClient) { }
   roles:Rol[] = [];
-  //URL_API='http://127.0.0.1:8000/api/auth';
+  URL_API_LOCAL='http://127.0.0.1:8000/api/auth';
   URL_API='https://apiser-vicios.herokuapp.com/api/auth';
 
   obtenerRoles(){
@@ -23,6 +25,31 @@ export class RolService {
   /**ACTUALIZAR URL CUANDO SE DEPLOYE EL BACKEND */
   editarRol(rol: any){
     return this.http.put<any>('http://apiser-vicios.herokuapp.com/api/auth/roles',rol);
+  }
+
+  /*eliminarRol(rol: any){
+    console.log(rol);
+    return this.http.delete<any>('http://127.0.0.1:8000/api/auth/roles',rol);
+  }*/
+
+  eliminarRol(id: string): Observable<any> {
+    return this.http.delete<any>(this.URL_API_LOCAL + '/roles/' + id)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = error.error.message;
+    }
+    console.log("error del servicio");
+    return throwError(errorMessage);
   }
 
 }
