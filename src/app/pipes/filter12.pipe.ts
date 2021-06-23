7015a9e3fd5c6f857a9aa6ee438790a9338a10f4
Neo <enrique.vallejos.777@gmail.com>
2021-06-23 19:27:31 -0400

    Agregando token

---
+++ src/app/pipes/filter12.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter12'
+})
+export class Filter12Pipe implements PipeTransform {
+
+  transform(value: any, args: any): any {
+    const resultPosts =[] as  any;
+    for(const solicitud of value){
+      if( solicitud.montoestimado.toString().indexOf(args)>-1
+      ||solicitud.estado.toLowerCase().indexOf(args.toLowerCase())>-1
+      ||solicitud.created_at.toString().indexOf(args.toLowerCase())>-1
+      ||solicitud.tipo.toLowerCase().indexOf(args.toLowerCase())>-1
+      ){
+        resultPosts.push(solicitud);
+      }
+    } 
+    return resultPosts;
+  }
+
+
+
+}
