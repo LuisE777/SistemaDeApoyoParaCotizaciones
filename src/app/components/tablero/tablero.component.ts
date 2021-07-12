4755f6894c219b1af46b4954652fa842c2c43ae9
JMauricioQuinonesVargas <201702063@est.umss.edu>
2021-07-12 14:03:23 -0400

    arreglado sidebar no cargaba correctamente

--- src/app/components/tablero/tablero.component.ts
+++ src/app/components/tablero/tablero.component.ts
@@ -10,18 +10,21 @@ export class TableroComponent implements OnInit {
 
   privilegios: string = "{\"admin\":false,\"jefe\":false,\"cotizador\":false,\"usuario\":false}";
   privilegios_json: any ='';
+  priv: any;
 
   constructor(public rolService: RolService) { }
 
   ngOnInit(): void {
     this.getPrivilegios();
     //this.privilegiosToJson();
+   
   }
 
   privilegiosToJson(){    
     try{
       this.privilegios_json = JSON.parse(this.privilegios);
       localStorage.setItem('privilegios', JSON.stringify(this.privilegios_json));
+      this.priv = localStorage.getItem('privilegios');
       console.log(this.privilegios_json);
     } catch(error){
       console.log(error);
@@ -35,6 +38,7 @@ export class TableroComponent implements OnInit {
         console.log("RESULTADO",res[0].privilegios);
         this.privilegios=res[0].privilegios;
         this.privilegiosToJson();
+        //
       },
       err => console.log('')
     )
